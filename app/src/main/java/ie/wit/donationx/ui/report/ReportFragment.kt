968f9bb7bdc7846ebe453b499d83e0971ac99d7b
Jakub Poczatek <kubapoczatek2001@gmail.com>
2023-01-27 09:08:16 +0000

    Using SafeArgs
    Complete.

--- app/src/main/java/ie/wit/donationx/ui/report/ReportFragment.kt
+++ app/src/main/java/ie/wit/donationx/ui/report/ReportFragment.kt
@@ -84,7 +84,7 @@ class ReportFragment : Fragment(), DonationClickListener {
     }
 
     override fun onDonationClick(donation: DonationModel) {
-        val action = ReportFragmentDirections.actionReportFragmentToDonationDetailFragment()
+        val action = ReportFragmentDirections.actionReportFragmentToDonationDetailFragment(donation.id)
         findNavController().navigate(action)
     }
 }
